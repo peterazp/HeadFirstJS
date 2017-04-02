e06dc32a1ad9be7b930b890ff713db14b92b1d35
aspus <aspus@DESKTOP-C4USUBA>
2017-04-02 16:22:11 +0200

    Adding new files

---
+++ HeadFirstJS/bethebrowser1.js
@@ -0,0 +1,13 @@
+// Test for jokes
+
+var joke = "JavaScript walked into a bar";
+var toldJoke = false;
+var $punchline = "Better watch out for those semicolons.";
+var entage = 20;
+var result;
+
+if (toldJoke == true) {
+	alert($punchline);
+}	else {
+	alert(joke);	
+}

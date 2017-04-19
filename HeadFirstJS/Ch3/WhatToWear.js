59a44dde59150771583d5175f25de3798f036b2f
aspus <aspus@DESKTOP-C4USUBA>
2017-04-19 12:04:14 +0200

    New file

---
+++ HeadFirstJS/Ch3/WhatToWear.js
@@ -0,0 +1,23 @@
+/*function WhatShallIWear(temp) {
+	if (temp < 60) {
+		console.log("Wear a Jacket");
+	
+	} else if (temp < 70) {
+		console.log("Wear a Sweater");
+	
+	} else {
+		console.log("Wear a T-Shirt");
+	
+	}
+
+}
+
+WhatShallIWear(50);
+WhatShallIWear(60);
+WhatShallIWear(70); */
+function doIt(param) {
+	param = 2;
+}
+var test = 1;
+doIt(test);
+console.log(test);

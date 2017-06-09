9b354f56aba3892bcac495ddea24d5e88bcf3039
aspus <aspus@DESKTOP-C4USUBA>
2017-06-09 08:55:35 +0200

    Adding new files

---
+++ HeadFirstJS/Ch4/codemagnet_for_loop.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+var products = ["Cho Cho Choclate", "Bubblegum", "Cake Bater", "Icy Mint"];
+// var hasBubbleGum = [false, false, false, true];
+
+for (var i = 0; i < products.length; i = i + 1) {
+	if(products[i] == "Bubblegum") {
+		console.log(products[i] + " contains Bubblegum");
+	}
+}
